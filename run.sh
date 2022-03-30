beedfe463e17bd5117235bf24559720c80dcaa51
lukescerri <lukescerri@protonmail.com>
2022-03-31 00:59:13 +0200

    moved scene to scenes

--- run.sh
+++ run.sh
@@ -15,11 +15,11 @@ set -e
 rm img_*.ppm || :
 if [[ "$1" == "quality" ]]
 then
-	./build/renderer -s scene.json -w 1280 -h 960 -sw 1664 -sh 1248 -a 2
+	./build/renderer -s scenes/bunny_kitten.json -w 1280 -h 960 -sw 1664 -sh 1248 -a 2
 elif [[ "$1" == "fast" ]]
 then
-	./build/renderer -s scene.json -w 300 -h 300 -d 0.5 -a 2
+	./build/renderer -s scenes/bunny_kitten.json -w 300 -h 300 -d 0.5 -a 2
 else
-	./build/renderer -s scene.json -w 150 -h 150 -d 0.5 -a 1
+	./build/renderer -s scenes/bunny_kitten.json -w 150 -h 150 -d 0.5 -a 1
 fi
 conversion
